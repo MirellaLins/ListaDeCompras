8edd46e41657fd330b272420eb62614090570a1c
MirellaLDS <mirella.lins@mercos.com>
2025-02-04 11:14:05 -0300

    Compartilhar viewmodel entre as telas

--- app/src/main/java/dev/mlds/listadecompras/MainActivity.kt
+++ app/src/main/java/dev/mlds/listadecompras/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.*
 import dev.mlds.listadecompras.ui.theme.ListaDeComprasTheme
 import dev.mlds.listadecompras.view.CadastroItemScreen
@@ -15,14 +16,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             ListaDeComprasTheme {
                 val navController = rememberNavController()
+                val viewModel: ListaDeComprasViewModel = viewModel()
 
                 NavHost(navController, startDestination = "lista") {
                     composable("lista") {
-                        ListaDeComprasScreen(modifier = Modifier, navController)
+                        ListaDeComprasScreen(modifier = Modifier, navController, viewModel)
                     }
                     composable("cadastro") {
                         CadastroItemScreen(navController) { name, value ->
-
+                            viewModel.addItem(name, value)
                         }
                     }
                 }
